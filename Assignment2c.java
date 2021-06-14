523e399f613656cea9b04edc5dde77f14eab8f66
Shivam Choudhary <shivam.choudhary.it@gmail.com>
2021-06-14 18:17:51 +0530

    Logical Operator

---
+++ Assignment2c.java
@@ -0,0 +1,9 @@
+public class Assignment2c{
+	public static void main(String args[]){
+	boolean a=true;
+	boolean b=false;
+	System.out.println(!a);
+	System.out.println(a|b);
+	System.out.println((!a&b)|(a&!b));
+}
+}
