f0fc89ee330041bf654f60d63d77931eae51586a
Shivam Choudhary <shivam.choudhary.it@gmail.com>
2021-06-14 17:10:09 +0530

    Test

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+	public static void main(String []args){
+		String firstName = args[0];
+		String lastName = args[1];
+		int A = Integer.parseInt(args[2]);
+		System.out.println("Welcome "+firstName+" "+lastName + " "+A );
+
+}
+}
