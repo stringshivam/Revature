6414bd07ab9cbc9b4fd7c75662948bc51566c207
Shivam Choudhary <shivam.choudhary.it@gmail.com>
2021-06-14 18:16:12 +0530

    foreach loop example

---
+++ Assignment1b.java
@@ -0,0 +1,9 @@
+public class Assignment1b{
+	public static void main(String args[]){
+	//for each loop example
+	int[] arr={1,2,3,4,5,6,7,8,9};
+	for(int number:arr){
+		System.out.println(arr);
+	}
+	}
+}
