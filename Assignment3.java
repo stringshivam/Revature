2bddd2104fd6b57ec60141540b18ca9a470df248
Shivam Choudhary <shivam.choudhary.it@gmail.com>
2021-06-14 18:18:32 +0530

    Using Scanner Class

---
+++ Assignment3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Assignment3{
+	public static void main(String args[]){
+	//Using Scanner Class
+	Scanner sc = new Scanner(System.in);
+	String firstName=sc.next();
+	String lastName=sc.next();
+	int age = sc.nextInt();
+	System.out.println(firstName+" "+lastName+" "+age);
+
+
+
+}
+}
